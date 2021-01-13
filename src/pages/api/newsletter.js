3457c2b1cefad1a8cbf6f3c29238e8ba2c78346e
romanakhatun <romanakhatun2004@gmail.com>
2021-01-13 14:13:15 +0600

    newsletter changes

--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 
 function getRequestParams(email) {
-    const API_KEY = process.env.MAILCHIMP_API_KEY;
-    const LIST_ID = process.env.MAILCHIMP_LIST_ID;
-    const DATACENTER = process.env.MAILCHIMP_API_KEY.split("-")[1];
+    // const API_KEY = process.env.MAILCHIMP_API_KEY;
+    // const LIST_ID = process.env.MAILCHIMP_LIST_ID;
+    // const DATACENTER = process.env.MAILCHIMP_API_KEY.split("-")[1];
+
+    // const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`;
+
+    // const data = {
+    //     email_address: email,
+    //     status: "subscribed",
+    // };
+
+    // // Api key needs to be encoded in base 64 format
+    // const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
+    // const headers = {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Basic ${base64ApiKey}`,
+    // };
+
+    const API_KEY = "633f606915c9e2c5d6d4d823d38e56ea-us7";
+    const LIST_ID = "9dc8738a08";
+    const DATACENTER = "633f606915c9e2c5d6d4d823d38e56ea-us7".split("-")[1];
 
     const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`;
 
